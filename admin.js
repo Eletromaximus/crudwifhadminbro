c10ecdfb8177d319cbdaf1fbcd9da35623f43db1
Max Milliano de Souza Machado <maxmillianox@protonmail.com>
2020-12-17 21:55:01 -0300

    Conexao com o mongoose concluida com sucesso, basta configurar adminbro

--- admin.js
+++ admin.js
@@ -1,16 +1,42 @@
-const AdminBro = require('admin-bro')
-const AdminBroExpress = require('@admin-bro/express')
+const mongoose = require ("mongoose");
 
-const adminBro = new AdminBro({
-  databases: [],
+const ProjectSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  completed: Boolean,
+  created_at: { type: Date, default: Date.now },
+});
+
+const Project = mongoose.model("Project", ProjectSchema);
+
+const AdminBro = require('admin-bro');
+const AdminBroExpress = require('@admin-bro/express');
+const AdminBroMongoose = require('@admin-bro/mongoose');
+
+AdminBro.registerAdapter(AdminBroMongoose);
+
+const adminBroOptions = new AdminBro({
+  resources: [Project],
   rootPath: '/admin',
-})
+});
 
-const router = AdminBroExpress.buildRouter(adminBro)
+const router = AdminBroExpress.buildRouter(adminBroOptions);
 
 const express = require("express");
 const server = express();
 
-server
-  .use(adminBro.options.rootPath, router)
-  .listen(5500, () => console.log("Server started"));
+server.use(adminBroOptions.options.rootPath, router);
+
+const run = async () => {
+  await mongoose.connect("mongodb://localhost/adminbroapp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  await server.listen(5500, () => console.log("Server started"));
+}
+
+run();
